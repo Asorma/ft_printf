bca5dd9bba936d58589e58af46c36b40eff35967
Asorma <asormaetx@gmail.com>
2024-01-02 14:02:20 +0100

    Create ft_puthex_fd.c

---
+++ libft/src/ft_put/ft_puthex_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-orma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 13:15:30 by ade-orma          #+#    #+#             */
+/*   Updated: 2023/01/01 13:18:21 by ade-orma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int  ft_puthex_fd(uintptr_t ptr, const char format, int fd, int flag)
+{
+	int	i;
+
+	i = 0;
+	if (format == 'p' && ptr == 0)
+		return (ft_putstr_fd("(nil)", 1));
+	if (format == 'X')
+	{
+		if (ptr >= 16)
+			i += ft_puthex_fd(ptr / 16, format, fd, 0);
+		i += ft_putchar_fd("0123456789ABCDEF"[ptr % 16], fd);
+	}
+	else if (format == 'x' || format == 'p')
+	{
+		if (format == 'p' && i == 0 && flag == 0)
+			i += ft_putstr_fd("0x", 1);
+		if (ptr >= 16)
+			i += ft_puthex_fd(ptr / 16, format, fd, 1);
+		i += ft_putchar_fd("0123456789abcdef"[ptr % 16], fd);
+	}
+	return (i);
+}
